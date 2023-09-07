1e53c4070671e6d4bfc4d9d2b71b31a84a05d151
xiao-ice <125686290@qq.com>
2023-09-07 16:09:49 +0800

    chore: configure eslint

--- next.config.js
+++ next.config.js
@@ -1,70 +1,59 @@
-const path = require("path");
-const withCSS = require("@zeit/next-css");
-const withSass = require("@zeit/next-sass");
-const UnoCSS = require("@unocss/webpack").default;
-const presetUno = require("@unocss/preset-uno").default;
+const path = require('node:path')
+const withCSS = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
 
 module.exports = withSass(
   withCSS({
     cssModules: true,
-    webpack: (config, options) => {
+    webpack: (config) => {
       config.resolve.alias = {
         ...config.resolve.alias,
-        "@": path.resolve(__dirname),
-        "~": path.resolve(__dirname),
-      };
-      return config;
-    }
-  })
-);
+        '@': path.resolve(__dirname),
+        '~': path.resolve(__dirname),
+      }
+      return config
+    },
+  }),
+)
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")]
-  }
-};
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 
-const regexEqual = (x, y) =>
-  x instanceof RegExp &&
-  y instanceof RegExp &&
-  x.source === y.source &&
-  x.global === y.global &&
-  x.ignoreCase === y.ignoreCase &&
-  x.multiline === y.multiline;
+function regexEqual(x, y) {
+  return x instanceof RegExp
+  && y instanceof RegExp
+  && x.source === y.source
+  && x.global === y.global
+  && x.ignoreCase === y.ignoreCase
+  && x.multiline === y.multiline
+}
 
 module.exports = {
   optimizeFonts: false,
   reactStrictMode: true,
-  webpack(config, context) {
-    config.plugins.push(UnoCSS({ presets: [presetUno()] }));
-
-    if (context.buildId !== "development") {
-      // * disable filesystem cache for build
-      // * https://github.com/unocss/unocss/issues/419
-      // * https://webpack.js.org/configuration/cache/
-      config.cache = false;
-    }
-
-
-    const sassRules = config.module.rules.find((rule) => typeof rule.oneOf === "object").oneOf.find((rule) =>
-      rule.sideEffects === false &&
-      regexEqual(rule.test, /\.module\.(scss|sass)$/)
-    );
-    sassRules.use = sassRules.use.map((rule) =>
-      rule.loader.includes("sass-loader")
+  webpack(config) {
+    const sassRules = config.module.rules.find(rule => typeof rule.oneOf === 'object').oneOf.find(rule =>
+      rule.sideEffects === false
+      && regexEqual(rule.test, /\.module\.(scss|sass)$/),
+    )
+    sassRules.use = sassRules.use.map(rule =>
+      rule.loader.includes('sass-loader')
         ? {
-          ...rule,
-          options: {
-            ...rule.options,
-            additionalData: "@import './styles/common/index.scss';",
-          },
-        }
-        : rule
-    );
+            ...rule,
+            options: {
+              ...rule.options,
+              additionalData: '@import \'./styles/common/index.scss\';',
+            },
+          }
+        : rule,
+    )
 
     /* return new config to next */
-    return config;
+    return config
   },
 
-  distDir: "dist",
-};
+  distDir: 'dist',
+}
